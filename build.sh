e0ad83098f858483bb11f0bde65c8b0f82225d90
Adrian Sutton <adrian.sutton@ephox.com>
2010-06-14 13:13:43 +0100

    ADDED: Ant build process.

--- build.sh
+++
@@ -1,4 +0,0 @@
-#/bin/bash
-javac -classpath lib/jsobject.jar -d classes src/main/java/com/ephox/jsrobot/JSRobot.java && \
-jar cvf JSRobot.jar -C classes com && \
-jarsigner -keystore $STORE -storetype pkcs12 -storepass password -keypass password JSRobot.jar d9d674e4-3c59-42ca-b5ce-5e1fdf5158d6
